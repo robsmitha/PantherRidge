37784fc6c51762b7e4a91d2811b1df9b311ed4f3
robsmitha <robsmitha94@gmail.com>
2025-02-15 17:15:29 -0500

    fix iframe ts error

--- clientapp/src/components/AboutMe.vue
+++ clientapp/src/components/AboutMe.vue
@@ -63,7 +63,7 @@
             cover
             src="https://smitha-cdn.s3.us-east-2.amazonaws.com/PantherRidge/about.jpg">
           </v-img> -->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3541.420018548346!2d-82.32948999999999!3d27.425017999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c336cec83c910d%3A0xcbd62a1e7ce0d07e!2s6418%20209th%20St%20E%2C%20Bradenton%2C%20FL%2034211!5e0!3m2!1sen!2sus!4v1739656817585!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3541.420018548346!2d-82.32948999999999!3d27.425017999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c336cec83c910d%3A0xcbd62a1e7ce0d07e!2s6418%20209th%20St%20E%2C%20Bradenton%2C%20FL%2034211!5e0!3m2!1sen!2sus!4v1739656817585!5m2!1sen!2sus" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </v-col>
       </v-row>
     </v-container>
